690f8c2117f7aac1c1e862c3e5c138075aa15615
root <root@ip-172-31-34-81.us-east-2.compute.internal>
2021-05-27 12:53:42 +0000

    kjhskhjs

--- bob2.js
+++ bob2.js
@@ -57,8 +57,8 @@ const puppeteer = require('puppeteer');
     const preurl = 'https://twitter.com/search?q=crypto&src=typed_query&f=user'
     const browser = await puppeteer.launch({
       args: [
-      '--incognito',
-      '--no-sandbox']
+      '--no-sandbox',
+      '--user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"']
     });
     const page = await browser.newPage()
     //let body = await page.evaluate(() => document.body.innerHTML)
